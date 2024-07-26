301527bd37bff3f2349bd3be2e7e390c46435a0e
Guilherme Dias <guilherme.araujo97@Outlook.com>
2024-07-25 22:54:48 -0300

    Criacao da View Login finalizada

---
+++ Models/Login.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Projeto_Sistema_de_Vendas.Models
+{
+    public class Login : Controller
+    {
+        
+    }
+}
